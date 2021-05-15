c52c55fc0614a863fafd1e608506e862efa69ada
Israel Domiciano <israeldomiciano@gmail.com>
2021-05-15 12:49:03 -0300

    .211

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  larguraImagem: number = 80;
+  margemImagem: number = 2;
+  mostrarImagem: boolean = true;
+  filtroLista: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  alterarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
   public getEventos(): void {
